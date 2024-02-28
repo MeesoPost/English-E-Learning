cb2be83483b3cb0c55c803666d4c886dbe13d5d6
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2024-02-28 11:12:40 +0100

    refined pageheader component and styles

--- src/app/Components/PageHeader/PageHeader.tsx
+++ src/app/Components/PageHeader/PageHeader.tsx
@@ -5,15 +5,22 @@ interface PageHeaderProps {
     title: string;
     links: { label: string; url: string }[];
     children?: ReactNode;
+    className?: string; // Optional className for the header
+    linkClassName?: string; // Optional className for links
+    buttonClassName?: string; // Optional className for buttons
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title, links, children }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, links, children, className, linkClassName }) => {
     return (
-        <header className='page-header'>
+        <header className={`page-header ${className || ''}`}> 
             <h1>{title}</h1>
             <nav className='nav' >
                 {links.map((link, index) => (
-                    <a key={index} href={link.url}>
+                    <a 
+                        key={index} 
+                        href={link.url}
+                        className={linkClassName} // Apply to links
+                    >
                         {link.label}
                     </a>
                 ))}
